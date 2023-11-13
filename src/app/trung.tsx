949cbd574c763cf481352fe2a18e5ad26979a243
daulenapa <dau.le@napaglobal.com>
2023-11-13 17:25:15 +0700

    'init-project'

---
+++ src/app/trung.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type Props = {};
+
+export const trung: React.FC<Props> = (props) => {
+  return <div><Button>asdas</Button></div>;
+};
