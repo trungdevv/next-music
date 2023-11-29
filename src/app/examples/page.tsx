0e0604a87852d00ecef805fea917a273a583c303
daulenapa <dau.le@napaglobal.com>
2023-11-29 17:07:24 +0700

    next-auth

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -1,9 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
 
-
-export default function Hom1e() {
-  return (
-    <main className="">
-      1
-    </main>
+export default async function Example() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    { db: {schema: "next-auth"} }
   );
+  const { data, error } = await supabase.from("user").select()
+  return <pre>{JSON.stringify(data, null, 2)}</pre>
 }
