c633d6a40215cbaf905a596b013d2e4152f00e7e
trung.tran <trantrungse1209@gmail.com>
2023-12-06 15:57:21 +0700

    [Feat:] Update next-auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,38 @@
 import type { NextAuthOptions } from "next-auth";
-
-export const options : NextAuthOptions = {
-    providers : [],
-}
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          className : "text-red",
+          placeholder: "Enter your name",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials,req) {
+        const user = { id: 12, name: "admin", password: "admin" };
+        if (
+          credentials?.username === user.name && credentials?.password === user.password
+        ) {
+          return user;
+        }else{
+            return null
+        }
+        
+      },
+    }),
+  ],
+};
--- src/app/trung.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
-
-type Props = {};
-
-export const trung: React.FC<Props> = (props) => {
-  return <div><Button>asdas</Button></div>;
-};
