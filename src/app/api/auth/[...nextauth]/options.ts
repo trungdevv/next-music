4bb32dfa670e548202abf86c777cebb46dbe9a9c
trung.tran <trantrungse1209@gmail.com>
2023-12-06 15:57:21 +0700

    next-auth update

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,38 +1,21 @@
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
+
 export const options: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          className : "text-red",
-          placeholder: "Enter your name",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Enter your password",
-        },
-      },
-      async authorize(credentials,req) {
-        const user = { id: 12, name: "admin", password: "admin" };
-        if (
-          credentials?.username === user.name && credentials?.password === user.password
-        ) {
-          return user;
-        }else{
-            return null
-        }
-        
-      },
-    }),
   ],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname === "/middleware-example") {
+        console.log(1);
+        return !!auth
+      }
+      return true
+    },
+  },
 };
---
+++ src/app/examples/page.tsx
@@ -0,0 +1,12 @@
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/user-auth-form";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function Hom1e() {
+  return (
+    <main className="">
+      1
+    </main>
+  );
+}
